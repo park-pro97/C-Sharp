195537d5211d556ef680858ae31560bda5709cc2
park-pro97 <qazxc7093@naver.com>
2024-07-02 09:26:08 +0900

    Update Practice.cs

--- Week2/Day2/Practice.cs
+++ Week2/Day2/Practice.cs
@@ -1,1 +1,21 @@
+//do while문으로 구구단 출력
+namespace DoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            do
+            {
+                Console.WriteLine($"3 * {i} = {3 * i} ");
+                i++;
+            } while (i < 10);
+            
+        }
+    }
+}
+
+-------------------------------------------------------------------
+//메뉴 선택
 
