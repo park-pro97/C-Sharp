559a3bc8d4d2219520d69afe2f5afdf74b6bbd9c
park-pro97 <qazxc7093@naver.com>
2024-07-05 14:35:30 +0900

    Update Practice.cs

--- Week2/Day9/Practice.cs
+++ Week2/Day9/Practice.cs
@@ -226,3 +226,72 @@ namespace LottoApp03
         }
     }
 }
+
+
+---------------------------------------------------------------------------------------------------------------
+//오름차순 버블정렬
+namespace BubbleSortApp01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //오름차순 버블정렬
+          
+            int[] list = { 4, 5, 7, 3, 2, 1, 9, 8 };
+            //Array.Sort(arr);
+            int temp;
+            for(int i= list.Length - 1; i>0; i--)
+            {
+                for(int j=0; j<i; j++)
+                {
+                    if (list[j] > list[j+1])
+                    {
+                        //교환조건 이루어짐
+                        temp = list[j];
+                        list[j] = list[j+1];
+                        list[j+1] = temp;
+                    }
+                }
+            }
+            foreach(int i in list)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
+---------------------------------------------------------------------------------------------------------------
+//버블정렬 - for index가 커지는 방식으로 작성
+namespace BubbleSortApp02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //첫번째 for의 i값이 커지는 방식으로 작성한 버블정렬
+          
+            int[] list = { 4, 5, 7, 3, 2, 1, 9, 8 };
+            //Array.Sort(arr);
+            int temp;
+            for (int i = 0; i < list.Length - 1; i++)
+            {
+                for (int j = 0; j < list.Length - 1 - i; j++)
+                {
+                    if (list[j] > list[j + 1])
+                    {
+                        // 교환조건 이루어짐
+                        temp = list[j];
+                        list[j] = list[j + 1];
+                        list[j + 1] = temp;
+                    }
+                }
+            }
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
