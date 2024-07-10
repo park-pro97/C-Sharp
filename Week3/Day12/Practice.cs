8085cfaf6357ccd3599d782cc1c49f8782e8f1cd
park-pro97 <qazxc7093@naver.com>
2024-07-10 09:39:21 +0900

    Update Practice.cs

--- Week3/Day12/Practice.cs
+++ Week3/Day12/Practice.cs
@@ -1,4 +1,24 @@
-//
+//배열 연습
+namespace ArrayTest01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Numbers = new int[5] { 1, 2, 3, 4, 5 }; //배열을 선언과 동시에 초기화
+            //studentIDs[0] = 1;         //선언 후 초기화
+            string[] studentNames = new string[3] {"Ralo", "Paka", "Mouse"};
+
+            int[] evenNums = new int[10];
+            for(int x = 0; x < 10; x++)
+            {
+                evenNums[x] = x * 2;
+
+                Console.WriteLine($"You just saved {evenNums[x]}");
+            }
+        }
+    }
+}
 
 
 -------------------------------------------------------------------------------------------------------------------------------------------------
