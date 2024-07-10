f783effc450df38fc311b5d94572ad4fb0b4a51b
park-pro97 <qazxc7093@naver.com>
2024-07-10 17:17:44 +0900

    Update Practice.cs

--- Week3/Day12/Practice.cs
+++ Week3/Day12/Practice.cs
@@ -186,3 +186,38 @@ namespace WinFormsApp7
 
 ---------------------------------------------------------------------------------------------------------------------------------
 //UI 만들기3
+namespace Page56
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox2.Items.Add("서울특별시");
+            comboBox2.Items.Add("인천광역시");
+            comboBox2.Items.Add("대전광역시");
+            comboBox2.Items.Add("대구광역시");
+            comboBox2.Items.Add("부산광역시");
+            comboBox2.Items.Add("광주광역시");
+            comboBox2.Items.Add("울산광역시");
+            comboBox2.Items.Add("경기도");
+            comboBox2.Items.Add("충청도");
+            comboBox2.Items.Add("강원도");
+            comboBox2.Items.Add("경상도");
+            comboBox2.Items.Add("전라도");
+            comboBox2.Items.Add("제주도");
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(comboBox1.Text + textBox1.Text + "님은 " + comboBox2.Text + "에 거주하십니다.");
+        }
+    }
+}
