cf6dd869415ae5dccdac9890f8760c239bfa2259
park-pro97 <qazxc7093@naver.com>
2024-07-10 16:35:26 +0900

    Update Practice.cs

--- Week3/Day12/Practice.cs
+++ Week3/Day12/Practice.cs
@@ -99,3 +99,61 @@ namespace IndexText
     }
 }
 
+
+---------------------------------------------------------------------------------------------------------------------------------
+//UI만들기1
+
+---------------------------------------------------------------------------------------------------------------------------------
+//UI 만들기2
+
+namespace WinFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str;
+            str = "당신의 연령은 \n";
+            if (radioButton1.Checked == true)
+                str = str + radioButton1.Text;
+            if (radioButton2.Checked == true)
+                str = str + radioButton2.Text;
+            if (radioButton3.Checked == true)
+                str = str + radioButton3.Text;
+            if (radioButton4.Checked == true)
+                str = str + radioButton4.Text;
+            if (radioButton5.Checked == true)
+                str = str + radioButton5.Text;
+            if (radioButton6.Checked == true)
+                str = str + radioButton6.Text;
+
+            str = str + "\n" + "\n" + "좋아하는 색은" + "\n";
+
+            if(checkBox1.Checked == true)
+                str = str + checkBox1.Text + Environment.NewLine;
+            if (checkBox2.Checked == true)
+                str = str + checkBox2.Text + Environment.NewLine;
+            if (checkBox3.Checked == true)
+                str = str + checkBox3.Text + Environment.NewLine;
+            if (checkBox4.Checked == true)
+                str = str + checkBox4.Text + Environment.NewLine;
+            if (checkBox5.Checked == true)
+                str = str + checkBox5.Text + Environment.NewLine;
+            if (checkBox6.Checked == true)
+                str = str + checkBox6.Text + Environment.NewLine;
+
+            str = str + "입니다.";
+            label1.Text = str;
+        }
+    }
+}
