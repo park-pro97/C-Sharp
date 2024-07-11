2df3a1501ad2dced0e188a6da60495166e3a51e6
park-pro97 <qazxc7093@naver.com>
2024-07-11 13:59:57 +0900

    Update Practice.cs

--- Week3/Day13/Practice.cs
+++ Week3/Day13/Practice.cs
@@ -344,7 +344,31 @@ namespace Code117
     }
 }
 
-
+-----------------------------------------------------------------------------------------------------------------------
+//delegate
+namespace DelegateApp01
+{
+    //Delegate
+    //Code119
+    internal class Program
+    {
+        //delegate 선언
+        delegate void PrintDelegate(string str);
+        class Print
+        {
+            public void PrintOut(string str)
+            {
+                Console.WriteLine(str);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Print p = new Print();
+            PrintDelegate pdg = p.PrintOut;
+            pdg("안녕하세요");
+        }
+    }
+}
 -----------------------------------------------------------------------------------------------------------------------
 //delegate 사칙연산 (곱셈까지)
 namespace DelegateApp02
