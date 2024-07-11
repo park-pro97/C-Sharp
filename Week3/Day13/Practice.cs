93f23dad83f327d2d8873b476a136a569a890ce3
park-pro97 <qazxc7093@naver.com>
2024-07-11 11:15:25 +0900

    Update Practice.cs

--- Week3/Day13/Practice.cs
+++ Week3/Day13/Practice.cs
@@ -176,3 +176,44 @@ namespace OOPTest05
 }
 
 -----------------------------------------------------------------------------------------------------------------------
+//
+namespace PropertyTest
+{
+    class Product
+{
+    //private string name;
+    //private int price;
+
+    private int stock;
+    //Property
+    public string Name { get; set; }
+    public int Price { get; set; }
+    public int Stock
+    {
+        get { return stock; }
+        set
+        {
+            if (value < 0)
+            {
+                Console.WriteLine("재고가 없습니다.");
+            }
+            stock = value;
+        }
+    }
+    public Product(string name, int price, int stock)
+    {
+        Name = name;
+        Price = price;
+        Stock = stock;
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Product product = new Product("공책", 1000, 20);
+        product.Stock = -5;
+    }
+}
+}
