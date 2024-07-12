2527a789e74c4433155bfe43b9aacdda2dc8fdd7
park-pro97 <qazxc7093@naver.com>
2024-07-12 15:17:46 +0900

    Update Practice.cs

--- Week3/Day14/Practice.cs
+++ Week3/Day14/Practice.cs
@@ -161,6 +161,113 @@ namespace ListTestApp
 
 ----------------------------------------------------------------------------------------------------------
 //
+using System.Net;
+
+namespace MYddressBook
+{
+    internal class Program
+    {
+        class Address
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Phone { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            List<Address> list = new List<Address>();
+            int choice = 0;
+            do
+            {
+                Console.WriteLine("1. 데이터 삽입");
+                Console.WriteLine("2. 데이터 삭제");
+                Console.WriteLine("3. 데이터 조회");
+                Console.WriteLine("4. 데이터 수정");
+                Console.WriteLine("5. 프로그램 종료");
+                Console.Write("선택한 번호: ");
+                choice = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                switch (choice)
+                {
+                    case 1:
+                        InsertData(list);
+                        break;
+                    case 2:
+                        DeleteData(list);
+                        break;
+                    case 3:
+                        SearchData(list);
+                        break;
+                    case 4:
+                        UpdateData(list);
+                        break;
+                    case 5:
+                        Console.WriteLine("프로그램 종료.");
+                        break;
+                }
+            } while (choice != 5);
+        }
+        static void InsertData(List<Address> list)
+        {
+            Console.Write("ID를 입력하세요: ");
+            int Id = int.Parse(Console.ReadLine());
+            Console.Write("이름을 입력하세요: ");
+            string Name = Console.ReadLine();
+            Console.Write("전화번호를 입력하세요: ");
+            string Phone = Console.ReadLine();
+
+            list.Add(new Address { Id = Id, Name = Name, Phone = Phone });
+            Console.WriteLine("완료");
+            Console.WriteLine();
+        }
+        static void DeleteData(List<Address> list)
+        {
+            Console.Write("삭제할 ID를 입력하세요: ");
+            int Id = Int32.Parse(Console.ReadLine());
+            Address addressDelete = list.Find(a => a.Id == Id);
+            if (addressDelete != null)
+            {
+                list.Remove(addressDelete);
+                Console.WriteLine("삭제되었습니다.");
+            }
+            else
+            {
+                Console.WriteLine("ID를 찾을 수 없습니다.");
+            }
+            Console.WriteLine();
+        }
+        static void SearchData(List<Address> list)
+        {
+            foreach (var address in list)
+            {
+                Console.WriteLine($"ID               NAME              PHONE");
+                Console.WriteLine($"{address.Id}                {address.Name}            {address.Phone}");
+            }
+            Console.WriteLine();
+        }
+        static void UpdateData(List<Address> addressList)
+        {
+            Console.Write("수정할 ID를 입력해 주세요: ");
+            int id = Int32.Parse(Console.ReadLine());
+            Address addressToUpdate = addressList.Find(a => a.Id == id);
+            if (addressToUpdate != null)
+            {
+                Console.Write("새 이름을 입력해 주세요: ");
+                addressToUpdate.Name = Console.ReadLine();
+                Console.Write("새 전화번호를 입력해 주세요: ");
+                addressToUpdate.Phone = Console.ReadLine();
+                Console.WriteLine("데이터가 수정되었습니다.");
+            }
+            else
+            {
+                Console.WriteLine("ID를 찾을 수 없습니다.");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
 
 
 ----------------------------------------------------------------------------------------------------------
