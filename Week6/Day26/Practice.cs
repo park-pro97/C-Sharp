6d7f27aa78fed843711fc345b25a4f0d49cc9c5c
park-pro97 <qazxc7093@naver.com>
2024-07-30 14:18:37 +0900

    Update Practice.cs

--- Week6/Day26/Practice.cs
+++ Week6/Day26/Practice.cs
@@ -107,106 +107,49 @@ namespace DeepCopy
 ------------------------------------------------------------------------------
 //
 namespace TrapicLight03
-
 {
-
     public partial class Form1 : Form
-
     {
-
         public Form1()
-
         {
-
             InitializeComponent();
-
         }
-
-
-
- 
-
         private void Form1_Load(object sender, EventArgs e)
-
         {
-
             ChangeSingodoong(0);
-
         }
-
         public void ChangeSingodoong(int Color)
-
         {
-
             switch (Color)
-
             {
-
                 case 0:
-
                     pictureBox1.Image = Image.FromFile(System.Environment.CurrentDirectory + "/신호등(준비중).png");
-
                     break;
-
                 case 1:
-
                     pictureBox1.Image = Image.FromFile(System.Environment.CurrentDirectory + "/신호등(빨간색).png");
-
                     break;
-
                 case 2:
-
                     pictureBox1.Image = Image.FromFile(System.Environment.CurrentDirectory + "/신호등(노란색).png");
-
                     break;
-
                 case 3:
-
                     pictureBox1.Image = Image.FromFile(System.Environment.CurrentDirectory + "/신호등(녹색).png");
-
                     break;
-
             }
-
         }
-
         int sinhoodoong_Color = 1;
-
-
-
         bool flag = true;
-
         private void timer1_Tick(object sender, EventArgs e)
-
         {
-
             ChangeSingodoong(sinhoodoong_Color);
-
             if (sinhoodoong_Color == 3)
-
                 flag = false;
-
-
-
             else if (sinhoodoong_Color == 1)
-
                 flag = true;
-
-
-
             if (flag == true)
-
                 sinhoodoong_Color++;
-
-
-
             else if (flag == false)
-
                 sinhoodoong_Color--;
-
         }
-
     }
-
 }
 
