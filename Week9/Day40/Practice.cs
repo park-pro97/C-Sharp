71d7a9062b83ba97a1a7d31cb1900311620a72f2
park-pro97 <qazxc7093@naver.com>
2024-08-22 17:47:49 +0900

    Update Practice.cs

--- Week9/Day40/Practice.cs
+++ Week9/Day40/Practice.cs
@@ -67,23 +67,169 @@
 </html>
 
 -------------------------------------------------------------------
-//
+//dt_dd 이건 들여쓰기 같은 기능
+<!DOCTYPE html>
+<html lang="ko">
+    <head>
+        <meta charset="UTF-8">
+        <title>정의 목록</title>
+    </head>
+    <body>
+        <h3>계절별 양생화</h3>
 
+        <dl>
+            <dt>봄꽃</dt>
+            <dd>봄에 피느 꽃이 다년생 식물은 꽃잔디</dd>
+        </dl>
+
+        <dl>
+            <dt>여름꽃</dt>
+            <dd>여름에 피느 풀이 옥잠화, 비빛 수국</dd>
+        </dl>
+    </body>
+</html>
 -------------------------------------------------------------------
-//
+//링크 태그 [ 사진도 ]
+<!DOCTYPE html>
+<html>
+    <body>
+        <a href="dt_dd.html" target="_blank"></a>>dd페이지</a> <p></p>
 
+        <a href="./temp1/first.html" target="_blank">첫 웹페이지</a> <p></p>
+        <a href="sample.html" target="_blank">sample 웹페이지</a> <p></p>
+
+        <img src="img/a.png"> <p></p>
+        <img src="img/b.png" width="200" height"400"> <p></p>
+    </body>
+</html>
 -------------------------------------------------------------------
-//
-
+//테이블 만들어보기
+<!DOCTYPE html>
+<html>
+<head>
+    <title>표 그리기</title>
+    <style>
+        table, th, tr, td {
+            border:solid 1px black;
+            border-collapse: collapse;
+            padding: 8px;
+        }
+    </style>
+</head>
+    <body>
+        <table>
+            <tr>
+                <th>번호</th>
+                <th>이름</th>
+                <th>전화번호</th>
+            </tr>
+            <tr>
+                <th>1</th>
+                <th>홍길동</th>
+                <th>010-1111-1111</th>
+            </tr>
+            <tr>
+                <th>2</th>
+                <th>이명박</th>
+                <th>010-2222-2222</th>
+            </tr>
+        </table>
+    </body>
+</html>
 -------------------------------------------------------------------
-//
+//테이블3
+<!DOCTYPE html>
+<head>
 
+</head>
+<body>
+    <table border="1">
+        <tr>
+            <td>지역</td>
+            <td>현재기온</td>
+            <td colspan="2">불쾌지수/습도(%)</td>
+            
+            <td>풍속</td>
+        </tr>
+        <tr>
+            <td rowspan="2">서울/경기</td>
+            <td>23</td>
+            <td>60</td>
+            <td>80</td>
+            <td>4.7</td>
+        </tr>
+        <tr>
+            <td>20</td>
+            <td>60</td>
+            <td>80</td>
+            <td>5.0</td>
+        </tr>
+        <tr>
+            <td>제주도</td>
+            <td>21</td>
+            <td>65</td>
+            <td>85</td>
+            <td>3.6</td>
+        </tr>
+
+    </table>
+</body>
+</html>
 -------------------------------------------------------------------
-//
+//form 만들기
+<!DOCTYPE html>
+<html>
+<head>
 
+</head>
+<body>
+    <form>
+        이름 : <input type="text"><br>
+        나이 : <input type="text"><P>
+
+        비밀번호 : <input type="password"><br><br>
+        개인정보 : <input type="radio" name="info" checked> 공개
+                 <input type="radio" name="info"> 비공개
+                 <p></p>
+        이메일 : <input type="text">@
+                <SELECT>
+                    <OPTION>선택</OPTION>
+                    <OPTION>naver.com</OPTION>
+                    <OPTION>gmail.com</OPTION>
+                    <OPTION>daum.net</OPTION>
+                    <OPTION>직접입력</OPTION>
+                </SELECT>
+                <p></p>
+        자기소개 : <textarea rows="10" cols="50"></textarea><br>
+
+        <button type="button">검색</button>
+        <button type="submit">확인</button>
+        <button type="reset">다시 쓰기</button>
+    </form>
+</body>
+</html>
 -------------------------------------------------------------------
-//
-
+//생상 폰트 사이즈를 고정하고 작성
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>CSS 01</title>
+    <!-- CSS 코드를 삽입 가능 -->
+    <!-- 나중에 JQuery를 통해 프로그래밍화 -->
+    <style>
+        p {
+            color:red;
+            font-size: 30px;
+        }
+    </style>
+</head>
+<body>
+    <p>나무의 줄기는 땅 위로 높게 자라고 해마다 굵기가 두꺼워지지만,
+    풀의 줄기는 일 년만 자라고 겨울을 나는 동안 땅 윗부분이 죽는다.
+    </p>
+</body>
+</html>
 -------------------------------------------------------------------
 //
 
