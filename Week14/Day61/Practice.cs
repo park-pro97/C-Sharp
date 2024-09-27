481aaa1c59f700310123961efe6a924fd7e7b404
park-pro97 <qazxc7093@naver.com>
2024-09-27 19:43:29 +0900

    Update Practice.cs

--- Week14/Day61/Practice.cs
+++ Week14/Day61/Practice.cs
@@ -119,7 +119,7 @@ void loop() {
   delay(2000);
 }
 --------------------------------------------------------------
-// STEP 2
+// STEP 2   [ Broker가 실행 중인 Raspberry 보드에서는  Subscriber 프로그램이 Broker로 부터 온습도 정보 ]
 #file name : SubHumiTemp.py
 import paho.mqtt.client as mqtt
 
@@ -137,8 +137,35 @@ client.on_message = on_message
 client.connect(“localhost”, 1883, 60)
 client.loop_forever( )
 --------------------------------------------------------------
-// STEP 3
-
+// STEP 3 [ 온습도 정보를 구독하는 프로그램이 InfluxDB에 온습도 정보가 들어감 ]
+from influxdb import InfluxDBClient
+import paho.mqtt.client as mqtt
+dbClient = InfluxDBClient(host=‘localhost’, port=8086, username=‘influx_phirippa’, password=‘1234’,
+		database=‘db_riatech’)
+def on_connect( client, userdata, flags, rc ):
+   print(“Connect with result code “ + str(rc) )
+   client.subscribe(“Sensors/MyOffice/#”)
+def on_message( client, userdata, msg ):
+   print( msg.topic +” “+str(msg.payload) )
+   topic = msg.topic.split(‘/’)
+   json_body = [ ]
+   data_point = {
+	‘measurement’: ‘My_office’,
+	‘tags’: {‘Location’: ‘ ‘},
+	‘fields’: {‘Temp’: 0.0, ‘Humi’:0.0}
+	}
+   data_point[‘tags’][‘Location’] = topic[1]
+   data_point[‘tags’][‘SubLocation’] = topic[2]
+   data_point[‘fields’][topic[3]] = float(msg.payload)
+   json_body.append(data_point)
+   dbClient.write_points( json_body )
+   
+client = mqtt.Client( )
+client.username_pw_set(username=“mqtt_riatech”, password=“1234”)
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect(“localhost”, 1883, 60)
+client.loop_forever( )
 --------------------------------------------------------------
 //
 
