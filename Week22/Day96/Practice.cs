7ebe86b6c321dc03617b75d5e34b3a8698ae3a13
park-pro97 <qazxc7093@naver.com>
2024-11-24 17:21:14 +0900

    Update Practice.cs

--- Week22/Day96/Practice.cs
+++ Week22/Day96/Practice.cs
@@ -1,1 +1,26 @@
 다양한 조회기능
+SELECT * 
+FROM Code13 
+WHERE Jud1 = 'Fail'
+
+SELECT * 
+FROM Code13 
+WHERE Jud2 = 'Fail'
+
+SELECT * 
+FROM Code13 
+WHERE Jud3 = 'Fail'
+
+SELECT * 
+FROM Code13 
+WHERE TotalJud = 'Fail'
+
+SELECT * 
+FROM Code13 
+WHERE ModelID = 'Wafer A'
+
+SELECT * 
+FROM Code13 
+WHERE ModelID = 'Wafer B'
+
+날짜 별로 조회하는 기능 만들려고 하는데 코드 완성되면 Datetime.now로 LOG 출력하면서 그걸로 조회하게 만들 예정 
